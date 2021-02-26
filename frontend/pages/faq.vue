1ee6a2c63bac1fc5362a7974fe0cb43f3577d2cb
Patrik Vaďura <patrikvadura@icloud.com>
2021-02-26 15:57:26 +0100

    faq a terms stylizace a napojení

---
+++ frontend/pages/faq.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <HeroBasic
+      image="faq"
+      :title="$t('faq.title')"
+    />
+
+    <b-container class="contact d-flex justify-content-center">
+      <b-row>
+        <b-col>
+          <p>{{ $t('faq.description') }}</p>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    HeroBasic: () => import('~/components/hero/HeroBasic')
+  },
+
+  computed: {
+    description () {
+      return this.$t('global.metaDescription').substring(0, 67)
+    }
+  },
+
+  head () {
+    return {
+      title: this.$t('faq.title') + ' | NIOB FLUID s.r.o.',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.description + '...'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.contact {
+  padding: 4rem 0;
+}
+</style>
